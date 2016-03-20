9c54448fe78d36578a2121cc48d163fe9d733a05
patanric <patane_riccardo@gmx.net>
2016-03-20 23:48:36 +0100

    prometheus query

--- src/main/java/PrometheusRetriever.java
+++ src/main/java/PrometheusRetriever.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -8,8 +10,23 @@ public class PrometheusRetriever {
 
     public static void main (String[] args) throws IOException {
 
-        String urlPrometheus = "http://localhost:9090";
-        String paramPrometheus = "/api/v1/query?query=process_cpu_seconds_total&time=2016-02-27T23:52:00.000Z";
+        String host = "localhost";
+        int port = 9090;
+        long currTime = System.currentTimeMillis()/1000;
+        long startTime = currTime - 3600;
+        String step = "600s";
+        String query = "rate(process_cpu_seconds_total[30s])";
+
+
+        String urlPrometheus = "http://" + host + ":" + port;
+
+        String paramPrometheus = "/api/v1/query_range" +
+                "?query=" + query +
+                "&start=" + startTime +
+                "&end=" + currTime +
+                "&step=" + step;
+
+        System.out.println(paramPrometheus);
 
         HttpRequest con = new HttpRequest();
         List list = con.connect(urlPrometheus, paramPrometheus);
@@ -17,7 +34,6 @@ public class PrometheusRetriever {
         for (Object o : list) {
             System.out.println(o);
         }
-
 
     }
 
