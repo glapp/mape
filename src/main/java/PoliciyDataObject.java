f2703eadee01491230aee937d6d281a3996a3516
patanric <patane_riccardo@gmx.net>
2016-04-12 19:59:33 +0200

    created PolicyDataObject for Gson

--- src/main/java/PoliciyDataObject.java
+++ src/main/java/PoliciyDataObject.java
@@ -2,6 +2,13 @@ import java.util.List;
 
 public class PoliciyDataObject {
     private List<Rules> rules;
+
+    public List<Rules> getRules() {
+        return rules;
+    }
+    public void setRules(List<Rules> rules) {
+        this.rules = rules;
+    }
 }
 
 class Rules {
@@ -12,4 +19,47 @@ class Rules {
     String operator;
     String value;
     String id;
+
+    public String getApplication_id() {
+        return application_id;
+    }
+    public void setApplication_id(String application_id) {
+        this.application_id = application_id;
+    }
+    public String getMetric() {
+        return metric;
+    }
+    public void setMetric(String metric) {
+        this.metric = metric;
+    }
+    public String getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public String getOperator() {
+        return operator;
+    }
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
 }
