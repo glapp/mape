443349b77435c36b401a1c02014760c22ae04763
adrian.mwli@gmail.com <adrian.mwli@gmail.com>
2016-07-27 13:50:47 +0200

    updated MapeActionTypeHeuristic class

--- src/main/java/ch/uzh/glapp/mdp/MapeActionTypeHeuristic.java
+++ src/main/java/ch/uzh/glapp/mdp/MapeActionTypeHeuristic.java
@@ -57,32 +57,9 @@ public class MapeActionTypeHeuristic implements ActionType {
 			// if cost, ?
 			
 		
-//			for (String newProvider : PROVIDER_LIST) {
-//				for (String newRegion : REGION_LIST) {
-//					for (String newTier : TIER_LIST) {
-//						
-//						// check if a host from the new provider in the new region and new tier is available
-//						if (MdpUtils.isHostAvailable(newProvider, newRegion, newTier)) {
-//							
-////							System.out.println("Checking provider: " + newProvider + ", region: " + newRegion + ", tier: " + newTier);
-//						
-//							// Only generate action to move cell to higher tier host. This reduce the applicable actions from 35 to 12
-//							// given the following conditions:
-//							// (1) there are 4 available providers, 3 available tiers and 3 available regions to choose from
-//							// (2) current host is at tier 2
-//							if (MdpUtils.isNewTierHigherOrEqual(currentTier, newTier)) {
-////							if (!newProvider.equals(currentProvider) || !newRegion.equals(currentRegion) || !newTier.equals(currentTier)) {
-//								actionList.add(new MapeActionMove(cellName, newProvider, newRegion, newTier));
-//							} else {
-////								System.out.println("invalid action");
-//							}
-//						}
-//					}
-//				}
-//			}
 		}
 		
-		System.out.println("Size of move action list: " + actionList.size());
+		System.out.println("Size of heuristic action list: " + actionList.size());
 		
 		return actionList;
 	}
