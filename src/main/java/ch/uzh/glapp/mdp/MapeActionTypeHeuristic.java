70fb0c098817a28a68841ef9b81542f66ebe5627
adrian.mwli@gmail.com <adrian.mwli@gmail.com>
2016-07-27 16:07:04 +0200

    updated MapeActionTypeHeuristic class

--- src/main/java/ch/uzh/glapp/mdp/MapeActionTypeHeuristic.java
+++ src/main/java/ch/uzh/glapp/mdp/MapeActionTypeHeuristic.java
@@ -19,9 +19,16 @@ public class MapeActionTypeHeuristic implements ActionType {
 	public List<Action> allApplicableActions(State state) {
 		
 		List<Action> actionList = new ArrayList<Action>();
+		
+		// get violation details
+		MdpTriggerObject mdpTriggerObject = BasicBehaviorMape.getMdpTriggerObject();
 
-		List<ObjectInstance> cells = ((DeepOOState)state).objectsOfClass(CLASS_CELL);
-		for (ObjectInstance cell : cells) {
+		// get the cell ID of the violating cell (first one if there are multiple)
+		ObjectInstance cell = ((DeepOOState)state).object(mdpTriggerObject.getViolationList().get(0).getCellId());
+		
+//		List<ObjectInstance> cells = ((DeepOOState)state).objectsOfClass(CLASS_CELL);
+//		for (ObjectInstance cell : cells) {
+			
 			// String cellName, String provider, String region, String tier
 			
 			String cellName = ((MapeCell)cell).name();
@@ -29,9 +36,6 @@ public class MapeActionTypeHeuristic implements ActionType {
 			String currentRegion = ((MapeCell)cell).getRegion();
 			String currentTier = ((MapeCell)cell).getTier();
 			
-			// get violation details
-			
-			MdpTriggerObject mdpTriggerObject = BasicBehaviorMape.getMdpTriggerObject();
 			
 			// check how many violations
 			String violation = "";
@@ -39,21 +43,24 @@ public class MapeActionTypeHeuristic implements ActionType {
 //			String violation = mdpTriggerObject.getViolationList().get(0).getMetric();
 			}
 			
+			// default action: create another cell on the machine with same specification (provider, region, tier)
 			if (violation.equals("container_cpu_usage_seconds_total")) { // if CPU violation => move to bigger machine
 				if (currentTier.equals(TIER1)) {
 					if (MdpUtils.isHostAvailable(currentProvider, currentRegion, TIER2)) {
 						actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER2));
 					} else if (MdpUtils.isHostAvailable(currentProvider, currentRegion, TIER3)) {
 						actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER3));
+					} else {
+						actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, currentTier));
 					}
 				} else if (currentTier.equals(TIER2)) {
 					if (MdpUtils.isHostAvailable(currentProvider, currentRegion, TIER3)) {
 						actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER3));
+					} else {
+						actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, currentTier));
 					}
 				} else if (currentTier.equals(TIER3)) {
-					if (MdpUtils.isHostAvailable(currentProvider, currentRegion, TIER3)) {
-						actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, TIER3));
-					}
+					actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, currentTier));
 				}
 			} else if (violation.equals("memory_utilization")) { // if memory violation => move to bigger machine, other checking to decide if to create another cell
 				if (currentTier.equals(TIER1)) {
@@ -61,11 +68,17 @@ public class MapeActionTypeHeuristic implements ActionType {
 						actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER2));
 					} else if (MdpUtils.isHostAvailable(currentProvider, currentRegion, TIER3)) {
 						actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER3));
+					} else {
+						actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, currentTier));
 					}
 				} else if (currentTier.equals(TIER2)) {
-					actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER3));
+					if (MdpUtils.isHostAvailable(currentProvider, currentRegion, TIER3)) {
+						actionList.add(new MapeActionMove(cellName, currentProvider, currentRegion, TIER3));
+					} else {
+						actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, currentTier));
+					}
 				} else if (currentTier.equals(TIER3)) {
-					actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, TIER3));
+					actionList.add(new MapeActionCreate(cellName, currentProvider, currentRegion, currentTier));
 				}
 			} else if (violation.equals("cost")) {
 				
@@ -78,7 +91,7 @@ public class MapeActionTypeHeuristic implements ActionType {
 			// => if there are more than 2 cells => delete a cell
 			
 		
-		}
+//		}
 		
 		System.out.println("Size of heuristic action list: " + actionList.size());
 		
